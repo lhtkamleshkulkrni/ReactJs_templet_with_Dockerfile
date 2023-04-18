6a14fc99e85b3c71240b1dbf6c8376bfc871680a
Kamlesh Kulkarni <91947468+lhtkamleshkulkrni@users.noreply.github.com>
2023-04-18 10:23:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello from KK
+         Hello from Kamlesh
         </p>
         <a
           className="App-link"
